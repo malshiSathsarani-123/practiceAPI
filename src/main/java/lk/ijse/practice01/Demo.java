20752a538b1aede4329f5d3114c50826e95f740f
malshiSathsarani-123 <malshisathsarani78@gmail.com>
2024-02-03 11:08:05 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/practice01/Demo.java
@@ -0,0 +1,21 @@
+package lk.ijse.practice01;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "example", urlPatterns = "/example")
+public class Demo extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getHeader("name");
+        Integer contact = Integer.valueOf(req.getParameter("contact"));
+        System.out.println(name);
+        System.out.println(contact);
+    }
+}
